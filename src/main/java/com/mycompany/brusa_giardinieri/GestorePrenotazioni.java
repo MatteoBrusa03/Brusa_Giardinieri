3894c0ca01d4c5c367d75fc4d4f8e201bbb0714c
matte <matte@LAPTOP-QQS1MIF7>
2021-05-21 12:51:57 +0200

    Fatti metodi salvaCsv carica dati e salva dati infine inplementati nel main

---
+++ src/main/java/com/mycompany/brusa_giardinieri/GestorePrenotazioni.java
@@ -0,0 +1,118 @@
+package com.mycompany.brusa_giardinieri;
+
+
+import com.mycompany.brusa_giardinieri.Intervento;
+import eccezioni.FileException;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author matte
+ */
+public class GestorePrenotazioni 
+{
+    private Intervento[] prenotazioni;
+    private static int NUM_MAX_PRENOTAZIONI=100;
+    private int nPrenotazioni=0;
+    
+    //Costruttore.
+    public GestorePrenotazioni()
+    {
+        prenotazioni=new Intervento[NUM_MAX_PRENOTAZIONI];
+    }
+    
+    public GestorePrenotazioni(Intervento[] prenotazioni)
+    {
+        prenotazioni=new Intervento[NUM_MAX_PRENOTAZIONI];
+        
+        int numeroPrenotazioni=0;
+        
+        //se elencoLibri non contiene libri.
+        if(prenotazioni.length==0)
+            return;
+        
+        if(prenotazioni.length>getNumMaxPrenotazioni())
+            numeroPrenotazioni=getNumMaxPrenotazioni();
+        else
+            numeroPrenotazioni=prenotazioni.length;
+        
+        for(int i=0;i<numeroPrenotazioni;i++)
+        {
+            if(prenotazioni[i]!=null)
+            {
+                prenotazioni[i]=new Intervento(prenotazioni[i]);
+            }
+        }
+    }
+    
+    public static int getNumMaxPrenotazioni()
+    {
+        return NUM_MAX_PRENOTAZIONI;
+    }
+    
+    public int getNPrenotazioni()
+    {
+        return nPrenotazioni;
+    }
+    
+     public void aggiungiIntervento(Intervento intervento)
+    {
+        prenotazioni[nPrenotazioni]=new Intervento(intervento);  
+        nPrenotazioni++;
+    }
+     
+    public void esportaPrenotazioniCSV(String filePathName) throws IOException, FileException
+    {
+      Intervento intervento;
+      String stringaLibro;
+      TextFile f1= new TextFile(filePathName, 'W');
+      for (int i=0;i<getNPrenotazioni();i++)
+      {
+             intervento=getPrenotazione();
+             if (intervento!=null)
+             {
+                stringaLibro=i+";"+intervento.getCI()+";"+intervento.getNomeCliente()+";"+intervento.getIndirizzoCliente()+";"+intervento.getNomeGiardiniere()+";"+intervento.getCognomeGiardiniere()+";"+intervento.getDataInizioIntervento()+";"+intervento.getDataFineIntervento()+";";
+                f1.toFile(stringaLibro);
+            }
+      }
+      f1.close(); 
+  }
+  
+    public void salvaPrenotazione(String nomeFile) throws IOException
+    {   
+        FileOutputStream f1=new FileOutputStream(nomeFile);
+        ObjectOutputStream writer=new ObjectOutputStream(f1);
+        writer.writeObject(this);
+        writer.flush();
+        writer.close();   
+    }
+
+    public GestorePrenotazioni caricaPrenotazione(String nomeFile) throws IOException, FileException
+    {
+        GestorePrenotazioni i;
+        FileInputStream f1=new FileInputStream(nomeFile);
+        ObjectInputStream reader=new ObjectInputStream(f1);
+
+         try 
+         {
+             i=(GestorePrenotazioni)reader.readObject();
+             reader.close();
+             return i;
+         } 
+         catch (ClassNotFoundException ex) 
+         {
+             reader.close();
+             throw new FileException("Errore di lettura");
+         }   
+    }
+}
--- src/main/java/com/mycompany/brusa_giardinieri/Main.java
+++ src/main/java/com/mycompany/brusa_giardinieri/Main.java
@@ -5,6 +5,8 @@
  */
 package com.mycompany.brusa_giardinieri;
 
+import eccezioni.FileException;
+import java.io.IOException;
 import java.time.*;
 import java.util.*;
 
@@ -16,92 +18,157 @@ public class Main
 {
     public static void main(String[] args) 
     {
-       int sceltaUtente;
-       Scanner tastiera=new Scanner(System.in);
+       int sceltaUtente=-1;
        Intervento intervento;
+       GestorePrenotazioni g1=new GestorePrenotazioni();
+       Scanner tastiera=new Scanner(System.in);
        String nomeCliente;
        String indirizzoCliente;
        String nomeGiardiniere;
        String cognomeGiardiniere;
        int anno,mese,giorno,ora,minuto;
-       
+       String caricamentoDaFileOK;
+       String nomeFileTesto="Giardinieri.txt";
+       String nomeFileBinario="Prenotazioni.bin";
         
        String[] vociMenu=new String[8]; 
        vociMenu[0]="Esci";
        vociMenu[1]="Aggiungi prenotazine";
-       vociMenu[2]="termina prenotazione";
+       vociMenu[2]="Termina prenotazione";
        vociMenu[3]="Elimina prenotazione";
        vociMenu[4]="Visualizza prenotazioni di una certa data";
        vociMenu[5]="Visualizza prenotazioni in ordine cronologico inverso";
        vociMenu[6]="Esporta in CSV";
-       vociMenu[7]="Esporta in file binario";
+       vociMenu[7]="Salva Dati";
+       
+       try 
+        {
+            g1=g1.caricaPrenotazione(nomeFileBinario);
+            System.out.println("Dati caricati correttamente");
+        } 
+        catch (IOException ex) 
+        {
+            System.out.println("Impossibile accedere al file in lettura. I dati non sono stati caricati");
+        } 
+        catch (FileException ex) 
+        {
+            System.out.println(ex.toString());
+        }
        
        Menu menu=new Menu(vociMenu);
        
        do
        {
-         sceltaUtente=menu.sceltaMenu();
-         
-         switch(sceltaUtente)
+         try
          {
-             case 0:
-             {
-                 System.out.println("L'applicazione verrà terminata");
-                 break;
-             }            
-              
-             case 1:
-             {
-                 System.out.println("Aggiungi Prenotazione: ");
-                 System.out.println("Nome del cliente --> ");
-                 nomeCliente=tastiera.nextLine();
-                 System.out.println("Indirizzo del cliente --> ");
-                 indirizzoCliente=tastiera.nextLine();
-                 System.out.println("Nome del giardiniere --> ");
-                 nomeGiardiniere=tastiera.nextLine();
-                 System.out.println("Cognome del giardiniere --> ");
-                 cognomeGiardiniere=tastiera.nextLine();
-                 //LocalDateTime per l'inizio dell'intervento
-                 System.out.println("Anno prenotazione --> ");
-                 anno=tastiera.nextInt();
-                 System.out.println("Mese prenotazione --> ");
-                 mese=tastiera.nextInt();
-                 System.out.println("Giorno prenotazione --> ");
-                 giorno=tastiera.nextInt();
-                 System.out.println("Ora prenotazione --> ");
-                 ora=tastiera.nextInt();
-                 System.out.println("Minuto prenotazione --> ");
-                 minuto=tastiera.nextInt();
-                 System.out.println("Operazione ok");
-                 System.out.println("Premi un pulsante per continuare");
-                 tastiera.nextLine();
-                 break;
-             }
-             
-             case 2:
-             {
-                 System.out.println("Operazione ok");
-                 System.out.println("Premi un pulsante per continuare");
-                 tastiera.nextLine();
-                 break;
-             }
-             
-             case 3:
-             {
-                 System.out.println("Operazione ok");
-                 System.out.println("Premi un pulsante per continuare");
-                 tastiera.nextLine();
-                 break;
-             }
-             
-             case 4:
-             {
-                 System.out.println("Operazione ok");
-                 System.out.println("Premi un pulsante per continuare");
-                 tastiera.nextLine();
-                 break;
-             }
+           sceltaUtente=menu.sceltaMenu();
+         
+            switch(sceltaUtente)
+            {
+                case 0:
+                {
+                   System.out.println("L'applicazione verrà terminata");
+                   break;
+                }            
+
+                case 1:
+                {
+                   System.out.println("Aggiungi Prenotazione: ");
+                   System.out.println("Nome del cliente --> ");
+                   nomeCliente=tastiera.nextLine();
+                   System.out.println("Indirizzo del cliente --> ");
+                   indirizzoCliente=tastiera.nextLine();
+                   System.out.println("Nome del giardiniere --> ");
+                   nomeGiardiniere=tastiera.nextLine();
+                   System.out.println("Cognome del giardiniere --> ");
+                   cognomeGiardiniere=tastiera.nextLine();
+                   //LocalDateTime per l'inizio dell'intervento
+                   System.out.println("Anno prenotazione --> ");
+                   anno=tastiera.nextInt();
+                   System.out.println("Mese prenotazione --> ");
+                   mese=tastiera.nextInt();
+                   System.out.println("Giorno prenotazione --> ");
+                   giorno=tastiera.nextInt();
+                   System.out.println("Ora prenotazione --> ");
+                   ora=tastiera.nextInt();
+                   System.out.println("Minuto prenotazione --> ");
+                   minuto=tastiera.nextInt();
+                   System.out.println("Operazione ok");
+                   System.out.println("Premi un pulsante per continuare");
+                   tastiera.nextLine();
+                   break;
+                }
+
+                case 2:
+                {
+                   System.out.println("Operazione ok");
+                   System.out.println("Premi un pulsante per continuare");
+                   tastiera.nextLine();
+                   break;
+                }
+
+                case 3:
+                {
+                   System.out.println("Operazione ok");
+                   System.out.println("Premi un pulsante per continuare");
+                   tastiera.nextLine();
+                   break;
+                }
+
+                case 4:
+                {
+                   System.out.println("Operazione ok");
+                   System.out.println("Premi un pulsante per continuare");
+                   tastiera.nextLine();
+                   break;
+                }
+
+                case 5:
+                {
+                   System.out.println("Operazione ok");
+                   System.out.println("Premi un pulsante per continuare");
+                   tastiera.nextLine();
+                   break;
+                }
+
+                case 6:
+                {
+                  try
+                   {
+                       g1.esportaPrenotazioniCSV(nomeFileTesto);
+                       System.out.println("Libri esportati correttamente");
+                   }
+                   catch(IOException e1)
+                   {
+                       System.out.println("Impossibile accedere al file");
+                   }
+                   catch(FileException e2)
+                   {
+                      System.out.println(e2.toString());
+                   }
+                   break;
+                }
+
+                case 7:
+                {
+                   try 
+                   {
+                       g1.salvaPrenotazione(nomeFileBinario);
+                       System.out.println("Dati salvati correttamente");
+                   } 
+                   catch (IOException ex) 
+                   {
+                      System.out.println("Impossibile accedere al file in scrittura");
+                   }
+                }
+            }
          }
+         
+         catch (InputMismatchException | NumberFormatException e1 )
+            {
+                tastiera.nextLine();
+                System.out.println("Input non corretto");
+            }
          
        }while(sceltaUtente!=0);
     }
